cebfc7ea268658d3eed5c33f4c44d32f81627a37
terufumi1122 <terufumi1122@gmail.com>
2020-02-25 16:16:01 +0900

    chore:create user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :user do
+    # Gimei参考リンク（https://github.com/willnet/gimei）
+    gimei = Gimei.name
 
+    # Faker参考リンク（https://github.com/faker-ruby/faker）
+    password = Faker::Internet.password(min_length: 8, max_length: 16)
+
+    first_name            { gimei.first.kanji }
+    last_name             { gimei.last.kanji }
+    email                 { Faker::Internet.email }
+    password              { password }
+    password_confirmation { password }
   end
 end
