9bd3491192a4de2f3182db87b1f4b72ca6c732b6
terufumi1122 <terufumi1122@gmail.com>
2020-02-25 13:33:26 +0900

    chore:fix_routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'boards#index'
 
   resources :users
-  resources :boards
-  resources :joins
+  resources :boards, shallow: true do
+    resources :join
+  end
 end
