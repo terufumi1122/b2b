d3e8e6a7c505d41ef9a489d7f76b88677dc7c0cc
terufumi1122 <terufumi1122@gmail.com>
2020-02-25 12:13:40 +0900

    chore:add_column name to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_025929) do
+ActiveRecord::Schema.define(version: 2020_02_25_030142) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_02_25_025929) do
     t.string "salt"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
