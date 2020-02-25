86502091df2437da49b312bc679b147264aea924
terufumi1122 <54193748+terufumi1122@users.noreply.github.com>
2020-02-25 17:00:45 +0900

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
     validates :last_name,  length: { maximum: 20 }
     validates :email,      uniqueness: { case_sensitive: true }
   end
+  # 以下sorcery公式wikiよりコピペ
+  validates :password,              length: { minimum: 3 },      if: -> { new_record? || changes[:crypted_password] }
+  validates :password,              confirmation: true,          if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true,              if: -> { new_record? || changes[:crypted_password] }
 end
