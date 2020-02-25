23aeb42e8d8c1e0d9e9b625a0d365a23447800ea
terufumi1122 <terufumi1122@gmail.com>
2020-02-25 16:40:32 +0900

    remove presense: true

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   with_options presence: true do
-    validates :first_name, presense: true, length: { maximum: 20 } 
-    validates :last_name,  presense: true, length: { maximum: 20 }
+    validates :first_name, length: { maximum: 20 } 
+    validates :last_name,  length: { maximum: 20 }
     validates :email,      uniqueness: { case_sensitive: true }
   end
 end
