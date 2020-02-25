140a24940d00dfa8842478596ffaad8acaeeb393
terufumi1122 <terufumi1122@gmail.com>
2020-02-25 16:39:05 +0900

    add validates presense and length

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   with_options presence: true do
-    validates :first_name
-    validates :last_name
-    validates :email, uniqueness: { case_sensitive: true }
+    validates :first_name, presense: true, length: { maximum: 20 } 
+    validates :last_name,  presense: true, length: { maximum: 20 }
+    validates :email,      uniqueness: { case_sensitive: true }
   end
 end
