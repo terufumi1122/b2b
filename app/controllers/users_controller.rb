cc9c519678db3eebb1154f4198e37c426bcebdb0
松野浩士 <amanojac52@gmail.com>
2020-02-25 22:05:20 +0900

    fix:yml修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,11 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
   def show
   end
 
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_path
-    else
-      render :new
-    end
   end
 
   def edit
@@ -26,11 +15,5 @@ class UsersController < ApplicationController
   end
 
   def destroy
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
--- spec/factories/boards.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :board do
-    title { "MyString" }
-    description { "MyText" }
-    picture { "MyString" }
-    user_id { 1 }
-  end
-end
