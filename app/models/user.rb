a56bec4158855bf8728db2af45cf3d48a2636068
terufumi1122 <terufumi1122@gmail.com>
2020-02-25 15:28:11 +0900

    chore:add_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+    validates :email, uniqueness: { case_sensitive: false }
+  end
 end
